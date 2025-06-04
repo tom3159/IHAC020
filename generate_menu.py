d16825950bd9b69d72cbb4bfc32317e49314d713
tom3159 <165210162+tom3159@users.noreply.github.com>
2025-06-04 16:16:25 +0200

    Update generate_menu.py

--- generate_menu.py
+++ generate_menu.py
@@ -176,15 +176,5 @@ main_menu() {
     esac
 }
 
-# Vérification que le dossier /home/$USER/cheatsheet existe
-if [ ! -d "/home/$USER/cheatsheet" ]; then
-    echo "Création du répertoire /home/$USER/cheatsheet..."
-    mkdir -p /home/$USER/cheatsheet
-fi
-
-# Copie de tous les fichiers .md vers /home/$USER/cheatsheet
-echo "Copie des fichiers .md vers /home/$USER/cheatsheet..."
-cp ./MD/*.md /home/$USER/cheatsheet/
-
 main_menu
 """)
